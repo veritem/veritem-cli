148b3c1e44cebee0a2d0a38234ca780f316d35f0
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-07 19:05:39 +0200

    ➕ deno args parser

--- deps.ts
+++ deps.ts
@@ -1,1 +1,10 @@
 export { assertEquals } from "https://deno.land/std@0.89.0/testing/asserts.ts";
+export { Args, parse } from "https://deno.land/std@$STD_VERSION/flags/mod.ts";
+export {
+  bold,
+  cyan,
+  green,
+  magenta,
+  red,
+  yellow,
+} from "https://deno.land/std/fmt/colors.ts";
